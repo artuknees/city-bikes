7988b5d6786fbcffbd1d1368270dbe4737085b2e
artuknees <artuknees@gmail.com>
2022-10-09 00:39:30 -0300

    Enhance home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,24 +1,39 @@
 import React , {useState} from 'react';
-import homeImage from '../../public/homeImage.webp';
-import Image from 'next/image';
+import Link from 'next/link';
+
 import styles from '../../styles/HomePage.module.scss';
 
 const HomePage = () => {
 
-    const [stations, setStations] = useState([]);
-    const [basicData,setBasicData] = useState('');
-    const [selectedIndex,setSelectedIndex] = useState('');
-
-
 	return (
         <div className={styles.generalContainer}>
-            <figure className={styles.figure}>
-                <Image src={homeImage} objectFit={'cover'} ></Image>
-            </figure>
-            {/* <h1>Bicicletas públicas</h1> */}
-            
+            <div className={styles.opacityContainer}>
+                <div className={styles.titleContainer}>
+                    <section className={styles.titleSpace}>
+                        
+                        <h1 className={styles.title}>Ride your bike through Buenos Aires City</h1>
+                        <p>Become a member, breeze through the boroughs, and explore the city you love</p>
+                        
+                        <div>
+                            <Link href="/stationsFinder">
+                                <button className={styles.searchButton}>Search your bike!</button>                        
+                            </Link>
+                        </div>
 
-
+                        <div className={styles.spanContainer}>
+                            <span>How it works?</span>
+                        </div>
+                        <div className={styles.spanContainer}>
+                            <span>Pricing</span>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <div className={styles.lastContainer}>
+                <div className={styles.footerContainer}>
+                    <p>Made with <b>♡</b> at Villa Urquiza</p>
+                </div>
+            </div>
         </div>
 	);
 }
