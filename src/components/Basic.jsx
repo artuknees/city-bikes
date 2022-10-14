8d766e23528a46455e1d209b564987079b32683d
artuknees <artuknees@gmail.com>
2022-10-14 20:36:29 -0300

    loading data enhance

--- src/components/Basic.jsx
+++ src/components/Basic.jsx
@@ -67,47 +67,48 @@ const Basic = () => {
 	return (
         <div className={styles.basicContainer}>
             <div className={styles.dataContainer}>
-                <h1>Find your bike or your empty slot</h1>
-                
-                {basicData ? <h3>{basicData}</h3> : <h3>Loading data...</h3>}
-
-                {/* <h5>{basicData ? basicData : 'Loading data...'}</h5> */}
-
-                {basicData ? <p> Stations = { stations ? stations.length : '' } </p>  : <Loading/> }
-                
-                { selectedIndex ? 
-
-                    <div className={styles.container}>
-                        {/* <button className={styles.button} onClick={() => fetchData()}> */}
-                        <button className={styles.button}>
-
-                            <span className={styles.span}>Refresh</span>
-                        </button>
-                    </div>
-                : null }
-
 
                 {basicData ? 
                     <>
-                    <span>Select a station: </span>
-                    <select defaultValue={'Seleccionar'} name="" id="selectName" onChange={(e) => selectStation(e.target.value)}>
-                    <option value='Seleccionar' disabled>Seleccionar</option>
-                    { [...stations].sort((a,b) => a.name.localeCompare(b.name)).map(item => <option value={item.index} key={item.index}>{item.name}</option>)  } 
-                    </select> 
-                    </>
-                    : null}
+                        <h1>Find your bike or your empty slot</h1>
+                        {basicData ? <p> Stations = { stations ? stations.length : '' } </p>  : null }
+                        { selectedIndex ? 
+                            <div className={styles.container}>
+                                <button className={styles.button}>
 
-                
-                {selectedIndex === '' ? null : 
-                    <div className={styles.results}>
-                        <p><b>Seleccionaste la estación:</b> { stations[selectedIndex].name }</p>
-                        <p><b>Dirección exacta:</b> { stations[selectedIndex].address }</p>
-                        <p><b>Bicicletas disponibles:</b> { stations[selectedIndex].free_bikes }</p>
-                        <p><b>Lugares libres:</b> { stations[selectedIndex].empty_slots }</p>
-                        <p><b>Última actualización:</b> { new Intl.DateTimeFormat('es-ES', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format((stations[selectedIndex].last_updated)*1000) }</p>
-                    </div>    
+                                    <span className={styles.span}>Refresh</span>
+                                </button>
+                            </div>
+                        : null }
+
+                        {basicData ? 
+                            <>
+                            <span>Select a station: </span>
+                            <select defaultValue={'Seleccionar'} name="" id="selectName" onChange={(e) => selectStation(e.target.value)}>
+                            <option value='Seleccionar' disabled>Seleccionar</option>
+                            { [...stations].sort((a,b) => a.name.localeCompare(b.name)).map(item => <option value={item.index} key={item.index}>{item.name}</option>)  } 
+                            </select> 
+                            </>
+                        : null}
+
+                        {selectedIndex === '' ? null : 
+                            <div className={styles.results}>
+                            <p><b>Seleccionaste la estación:</b> { stations[selectedIndex].name }</p>
+                            <p><b>Dirección exacta:</b> { stations[selectedIndex].address }</p>
+                            <p><b>Bicicletas disponibles:</b> { stations[selectedIndex].free_bikes }</p>
+                            <p><b>Lugares libres:</b> { stations[selectedIndex].empty_slots }</p>
+                            <p><b>Última actualización:</b> { new Intl.DateTimeFormat('es-ES', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format((stations[selectedIndex].last_updated)*1000) }</p>
+                            </div>    
+                        }
+
+                    </> 
+                    : <div>
+                        <h1>Loading data</h1>
+                        <div>
+                            <Loading/>
+                        </div>
+                    </div>
                 }
-
             </div>
         </div>
 	);
